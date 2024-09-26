9f2dbb3e72516792bb0417df7201d63c69a06f04
Yash Shelar <87018353+YashShelar007@users.noreply.github.com>
2024-09-26 04:39:23 -0700

    Added HomePage (My Trips)

--- voyage-ai/app/(tabs)/mytrip.jsx
+++ voyage-ai/app/(tabs)/mytrip.jsx
@@ -1,10 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { Colors } from "./../../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import StartNewTripCard from "../../components/MyTrips/StartNewTripCard";
 export default function MyTrip() {
+  const [userTrips, setUserTrips] = useState([]);
+
   return (
-    <View>
-      <Text>My Trip</Text>
-    </View>
+    <ScrollView
+      style={{
+        padding: 25,
+        paddingTop: 55,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "poppins-bold",
+            fontSize: 35,
+          }}
+        >
+          My Trips
+        </Text>
+        <TouchableOpacity>
+          <Ionicons name="add-circle" size={50} color="black" />
+        </TouchableOpacity>
+      </View>
+
+      {userTrips?.length == 0 ? <StartNewTripCard /> : null}
+      <View
+        style={{
+          height: 100,
+        }}
+      ></View>
+    </ScrollView>
   );
 }
